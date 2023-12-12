8c6b71024441fdcd3ddef88feff49c5f5447d156
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-12 11:09:14 +0100

    MainWindow.xaml.cs Read and Delete method works

--- WPF_Database2/MainWindow.xaml.cs
+++ WPF_Database2/MainWindow.xaml.cs
@@ -16,9 +16,53 @@ namespace WPF_Database2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AnimalService service;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.service = new AnimalService();
+            Read();
+        }
+
+        private void Read()
+        {
+            dataGridAnimals.ItemsSource = this.service.GetAll();
+        }
+
+        private void buttonCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Animal selected = dataGridAnimals.SelectedItem as Animal;
+            if (selected == null)
+            {
+                MessageBox.Show("Törléshez válasszon ki egy állatot!");
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show($"Biztosan törölni szeretné az alábbi állatot: {selected.Name}?",
+                "Törlés", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                if (this.service.Delete(selected.Id))
+                {
+                    MessageBox.Show("Sikeres törlés!");
+                }
+                else
+                {
+                    MessageBox.Show("Sikertelen törlés!");
+                }
+                Read();
+            }
         }
     }
 }
